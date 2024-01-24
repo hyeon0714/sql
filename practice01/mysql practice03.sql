3d92871510ef07a9c6ef5c7969ee4420134f3edf
user <user@DESKTOP-KVO6FOC>
2024-01-24 22:29:40 +0900

    1번 범위 수정

--- practice01/mysql practice03.sql
+++ practice01/mysql practice03.sql
@@ -1,6 +1,8 @@
 #직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을 조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세요.
-select	department_name
-		,employee_id
+select	employee_id,
+		first_name,
+        last_name,
+		department_name
 from employees e, departments d
 where e.department_id=d.department_id
 order by department_name, employee_id desc;
